5d68645ff3b8bf02a344a1c2f70d7c132f4267fa
Tom Watson <tom@tomjwatson.com>
2020-04-09 20:13:30 +0200

    Ensure emojis cant be added to  recent emojis twice

--- emote/user_data.py
+++ emote/user_data.py
@@ -5,8 +5,9 @@ import shelve
 
 DATA_DIR = os.path.join(Path.home(), '.local/share/Emote')
 SHELVE_PATH = os.path.join(DATA_DIR, 'user_data')
-DEFAULT_RECENT_EMOJIS = ['🙂', '😄', '❤️', '👍', '😡', '🔥', '🤣', '😍', '😭']
+DEFAULT_RECENT_EMOJIS = ['🙂', '😄', '❤️', '👍', '🤞', '🔥', '🤣', '😍', '😭']
 RECENT_EMOJIS = 'recent_emojis'
+MAX_RECENT_EMOJIS = 54
 
 
 # Ensure the data dir exists
@@ -19,7 +20,13 @@ def load_recent_emojis():
 
 
 def update_recent_emojis(emoji):
-    new_recent_emojis = [emoji] + load_recent_emojis()[:53]
+    recent_emojis = load_recent_emojis()
+
+    if emoji in recent_emojis:
+        recent_emojis.remove(emoji)
+        new_recent_emojis = [emoji] + recent_emojis[:MAX_RECENT_EMOJIS - 2]
+    else:
+        new_recent_emojis = [emoji] + recent_emojis[:MAX_RECENT_EMOJIS - 1]
 
     with shelve.open(SHELVE_PATH) as db:
         db[RECENT_EMOJIS] = new_recent_emojis
