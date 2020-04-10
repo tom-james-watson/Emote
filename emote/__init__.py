42baf138feb5f8e4981504bc8dd1e3f1f6321369
Tom Watson <tom@tomjwatson.com>
2020-04-10 14:55:37 +0200

    Prevent two app instances from being created

--- emote/__init__.py
+++ emote/__init__.py
@@ -1,3 +1,4 @@
+import sys
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Keybinder', '3.0')
@@ -5,22 +6,39 @@ from gi.repository import Gtk, Keybinder
 from emote import picker, css, emojis
 
 
-def create_picker():
-    picker.EmojiPicker(Keybinder.get_current_event_time())
+class EmoteApplication(Gtk.Application):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, application_id='com.tomjwatson.Emote',
+                         **kwargs)
+
+        self.activated = False
+
+    def start_daemon(self):
+        # Register global shortcut for invoking the emoji picker
+        Keybinder.init()
+        Keybinder.bind("<Ctrl><Alt>E", lambda x: self.create_picker_window())
+
+        css.load_css()
+        emojis.init()
+
+        self.activated = True
+
+        # Run the main gtk event loop - this prevents the app from quitting
+        Gtk.main()
+
+    def create_picker_window(self):
+        picker.EmojiPicker(Keybinder.get_current_event_time())
+
+    def do_activate(self):
+        if not self.activated:
+            print('Launching emote daemon')
+            self.start_daemon()
+        else:
+            print('Second instance launched')
+            self.create_picker_window()
 
 
 def main():
-    print('Launching emote daemon')
-
-    Gtk.Application(application_id='org.tom-james-watson.EmojiGTK')
-
-    css.load_css()
-
-    emojis.init()
-
-    # Register global shortcut for invoking the emoji picker
-    Keybinder.init()
-    Keybinder.bind("<Ctrl><Alt>E", lambda x: create_picker())
-
-    # Run the main gtk event loop - this prevents the app from quitting
-    Gtk.main()
+    app = EmoteApplication()
+    app.run(sys.argv)
