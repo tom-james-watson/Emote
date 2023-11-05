afbaed578672697f21dfc36ec53df7bede42ea20
Dexx Stexx <dchosen1uno@gmail.com>
2023-11-05 03:08:37 -0700

    Fix accidental deletion in config.py

--- emote/config.py
+++ emote/config.py
@@ -11,6 +11,6 @@ flatpak_root = os.environ.get("FLATPAK")
 is_wayland = os.environ.get("XDG_SESSION_TYPE", "").lower() == "wayland"
 
 try:
-    is_debian = platform.freedesktop_os_release()['ID'] == 'debian' or 
+    is_debian = platform.freedesktop_os_release()['ID'] == 'debian' or 'debian' in platform.freedesktop_os_release()['ID_LIKE']
 except:
     is_debian = False
